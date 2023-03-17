2448891445552247a648141cba4fb5aab5c183ce
Dominatrix1 <sleeksamurai@gmail.com>
2023-03-17 06:33:19 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a phrase 
+ * Return 0 (Success)*/
+int main(Void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return(0);
+}
