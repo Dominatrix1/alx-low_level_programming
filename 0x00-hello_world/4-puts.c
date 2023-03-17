80da13e934152892f6a1048c78f902b92176bb7d
Dominatrix1 <sleeksamurai@gmail.com>
2023-03-17 06:52:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
- * main - A program that prints a phrase
- * Return: 0 (Success)
+/*
+ * main - A program that prints a phrase with puts function
+ * return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0):
+return(0);
 }
